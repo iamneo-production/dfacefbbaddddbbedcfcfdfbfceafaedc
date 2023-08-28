6d56b43ee8e20379f51d6f7db0a2e216f5d76bb9
Iamneo <techadmin@iamneo.ai>
2023-08-28 09:47:01 +0000

    Backup Commit

--- springapp/src/main/java/com/examly/springapp/controller/TaskController.java
+++ springapp/src/main/java/com/examly/springapp/controller/TaskController.java
@@ -1,66 +1,47 @@
 package com.examly.springapp.controller;
 
+import com.examly.springapp.entity.Taskentity;
+import com.examly.springapp.service.TaskService;
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.examly.springapp.exception.ResourceNotFoundException;
-import com.examly.springapp.model.Task;
-import com.examly.springapp.repository.TaskRepository;
-
 @RestController
-@RequestMapping("")
-
+@RequestMapping()
 public class TaskController {
+    @Autowired
+    private TaskService service;
+    
+    @PostMapping("/saveTask")
+    public Taskentity saveTask(@RequestBody Taskentity taskentity){
+        Taskentity output = service.saveTask(taskentity);
+        return output;
+    }
 
- @Autowired
- private TaskRepository taskRepository;
 
- @GetMapping("/alltasks")
- public List<Task> getallTasks()
- {
- return taskRepository.findAll();
- }
- 
- @PostMapping("/saveTask")
- public Task createTask(@RequestBody Task task)
- {
- return taskRepository.save(task);
- }
+    @GetMapping("/changeStatus")
+    private Taskentity updatetaskStatus(@RequestParam("id") String id){
+        return service.updatetaskStatus(id);
+    }
 
- @GetMapping("/getTask")
- public ResponseEntity<Task> getTaskById(@RequestParam Long id)
- {
- Task task = taskRepository.findById(id).orElseThrow(()-> new ResourceNotFoundException("Task not found :"+id));
- return ResponseEntity.ok(task);
- }
+    @GetMapping("/deleteTask")
+    private String deleteTask(@RequestParam("id") String id){
+        return service.deleteTask(id);
+    }
+    @GetMapping("/alltasks")
+    private List<Taskentity> getallTasks(){
+        return service.getallTasks();
+    }
 
- @GetMapping("/changeStatus")
- public ResponseEntity<Task> updateTask(@RequestParam Long id, @RequestBody Task Taskdetails)
- {
- Task task = taskRepository.findById(id).orElseThrow(()-> new ResourceNotFoundException("Task not found :"+id));
- task.setTaskStatus(Taskdetails.getTaskStatus());
-
- Task updatedtask = taskRepository.save(task);
- return ResponseEntity.ok(updatedtask);
-
- }
-
- @GetMapping("/deleteTask")
- public String deleteTaskById(@RequestParam Long id)
- {
- Task task = taskRepository.findById(id).orElseThrow(()-> new ResourceNotFoundException("Task not found :"+id));
- taskRepository.delete(task);
- return "deleted sucessfully";
- }
+    @GetMapping("/getTask")
+    private Taskentity getTask(@RequestParam("id") String id){
+        return service.gettaskbyid(id);
+    }
 
 }
--- springapp/src/main/java/com/examly/springapp/repository/TaskRepository.java
+++ springapp/src/main/java/com/examly/springapp/repository/TaskRepository.java
@@ -1,11 +1,13 @@
 package com.examly.springapp.repository;
 
+import com.examly.springapp.entity.Taskentity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import com.examly.springapp.model.Task;
-
-@Repository
-public interface TaskRepository extends JpaRepository<Task,Long>{
+import java.util.Optional;
  
+@Repository
+public interface TaskRepository  extends JpaRepository<Taskentity,String>{
+Optional<Taskentity> findByTaskId(String taskid);
+void deleteByTaskId(String taskid);
+
 }
